c4a693fff62d6ac57b4298f7e133bca0ee81cc0b
izumi <rizumi222@gmail.com>
2025-02-11 16:39:03 +0900

    Add Navigation

--- ObservationExample/ContentView.swift
+++ ObservationExample/ContentView.swift
@@ -1,21 +1,26 @@
-//
-//  ContentView.swift
-//  ObservationExample
-//
-//  Created by izumi on 2025/02/11.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section("SwiftUI") {
+                    NavigationLink("Observation") {
+                        VStack {
+                            Text("Hello")
+                        }
+                    }
+                    NavigationLink("Combine") {
+                    }
+                }
+                Section("UIKit") {
+                    Button("Observation") {
+                    }
+                    Button("Combine") {
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
