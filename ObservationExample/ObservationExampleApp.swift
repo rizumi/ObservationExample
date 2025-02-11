4f64aed94f3541a6643fa86ad02f26d67c67324c
izumi <rizumi222@gmail.com>
2025-02-11 16:11:37 +0900

    Add project

---
+++ ObservationExample/ObservationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationExampleApp.swift
+//  ObservationExample
+//
+//  Created by izumi on 2025/02/11.
+//
+
+import SwiftUI
+
+@main
+struct ObservationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ObservationExampleTests/ObservationExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ObservationExampleTests.swift
+//  ObservationExampleTests
+//
+//  Created by izumi on 2025/02/11.
+//
+
+import Testing
+@testable import ObservationExample
+
+struct ObservationExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
